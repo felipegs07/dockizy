b3b33347d80568bbaa10bfddb88b87b9bcbdafed
Felipe <felipegdas07@gmail.com>
2019-04-11 17:07:15 -0300

    theme migration completed

--- themes/dockizy/source/js/opening.js
+++ themes/dockizy/source/js/opening.js
@@ -1,3 +1,3 @@
 var start = document.querySelector('#home a:first-child');
-if (start != null){ /*window.location.href = start;*/ }
+if (start != null){ window.location.href = start; }
 else { window.location.href = '/404' }
